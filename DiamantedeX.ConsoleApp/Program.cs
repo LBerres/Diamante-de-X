36f36c1c3a8d2c0837e07c8f4a3151a7ebae3c0b
LBerres <Leandro.motorhead@gmail.com>
2025-04-16 21:33:31 -0300

    Add project files.

---
+++ DiamantedeX.ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace DiamantedeX.ConsoleApp
+{
+    internal class Program
+    {/*
+      * Desenvolver um programa em que, dado um número ímpar como entrada,
+        exibirá um diamante desenhado com a letra 'X'.
+
+        ● O programa deve solicitar ao usuário um número inteiro.
+        ● O número deve ser ímpar. Caso contrário, o programa deve informar que
+        a entrada é inválida e solicitar um novo número.
+        ● O número de linhas e colunas deve ser igual ao número ímpar
+        fornecido.
+        ● A linha central do diamante deve conter o número máximo de Xs.
+     */
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
